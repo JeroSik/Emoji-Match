168a4db1f87c29d7889c8a74dfcc725070d19787
Youngsik Yoon <youyoon@Youngsiks-MBP.attlocal.net>
2019-05-16 23:33:13 -0500

    Added card flipping

--- Emoji Match/Emoji Match/ViewController.swift
+++ Emoji Match/Emoji Match/ViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction func touchSmileCard(_ sender: UIButton) {
+        flipCard(withEmoji: "😀", on: sender)
+    }
+    
+    @IBAction func touchGhostCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard withEmoji \(emoji)")
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.6392156863, green: 0.6666666667, blue: 0.6823529412, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 }
 
