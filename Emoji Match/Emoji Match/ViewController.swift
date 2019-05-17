d426b652c9db24d4e3f0b7ca52b2f877a5e1cb80
Youngsik Yoon <youyoon@Youngsiks-MBP.attlocal.net>
2019-05-16 20:35:22 -0500

    Initialized Project Structure

---
+++ Emoji Match/Emoji Match/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Emoji Match
+//
+//  Created by Youngsik Yoon on 5/16/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+    }
+}
+
---
+++ Emoji Match/Emoji MatchTests/Emoji_MatchTests.swift
@@ -0,0 +1,34 @@
+//
+//  Emoji_MatchTests.swift
+//  Emoji MatchTests
+//
+//  Created by Youngsik Yoon on 5/16/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import XCTest
+@testable import Emoji_Match
+
+class Emoji_MatchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
