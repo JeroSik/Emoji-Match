075fbde05bf11edd284e443503b3c6d14fe994e6
Youngsik Yoon <youyoon@Youngsiks-MacBook-Pro.local>
2019-05-17 10:34:52 -0500

    Added Classes

---
+++ Emoji Match/Emoji Match/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Emoji Match
+//
+//  Created by Youngsik Yoon on 5/17/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ Emoji Match/Emoji Match/Emoji Match.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji Match.swift
+//  Emoji Match
+//
+//  Created by Youngsik Yoon on 5/17/19.
+//  Copyright © 2019 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMatch {
+    var cards: Array<Card>
+    
+    func chooseCard(at Index: Int) {
+        
+    }
+}
